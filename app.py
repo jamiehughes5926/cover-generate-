d409052a21212b3bcaee84afbec365f2fca188d5
Macbook <jamiehughes5926@icloud.com>
2023-03-31 16:02:00 +1300

    first commit

---
+++ app.py
@@ -0,0 +1,75 @@
+import json
+import openai
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+openai.api_key = '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'
+model_id = 'gpt-3.5-turbo'
+
+def ChatGPT_conversation(conversation):
+    response = openai.ChatCompletion.create(
+        model=model_id,
+        messages=conversation
+    )
+    conversation.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
+    return conversation
+
+def read_resume(file_path):
+    with open(file_path, 'r') as file:
+        resume = file.read()
+    return resume
+
+def read_job_info(file_path):
+    with open(file_path, 'r') as file:
+        job_info = json.load(file)
+    return job_info
+
+def generate_cover_letter(resume, job_info):
+    conversation = [{'role': 'system', 'content': 'You are a cover letter generator.'}]
+    
+    conversation.append({'role': 'user', 'content': f"Generate a cover letter using the following resume:\n\n{resume}\n\nAnd the following job information:\n\n{json.dumps(job_info, indent=2)}"})
+    conversation = ChatGPT_conversation(conversation)
+    cover_letter = conversation[-1]['content'].strip()
+
+    return cover_letter
+
+def save_cover_letter(cover_letter, filename):
+    with open(filename, 'w') as file:
+        file.write(cover_letter)
+
+@app.route('/generate_cover_letter', methods=['POST'])
+def api_generate_cover_letter():
+    data = request.get_json()
+
+    if 'resume' not in data or 'job_info' not in data:
+        return jsonify({'error': 'Missing required data'}), 400
+
+    resume = data['resume']
+    job_info = data['job_info']
+
+    if 'jobTitle' not in job_info or 'companyTitle' not in job_info or 'jobDescription' not in job_info:
+        return jsonify({'error': 'Missing required job information'}), 400
+
+    job_data = {
+        "jobTitle": job_info['jobTitle'],
+        "companyName": job_info['companyTitle'],
+        "jobDescription": job_info['jobDescription']
+    }
+
+    cover_letter = generate_cover_letter(resume, job_data)
+    return jsonify({'cover_letter': cover_letter})
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    header['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
+    header['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import * as pdfjsLib from "pdfjs-dist";
+
+const pdfToText = async (file) => {
+  const reader = new FileReader();
+  const text = await new Promise((resolve) => {
+    reader.onload = async (event) => {
+      const pdfData = new Uint8Array(event.target.result);
+      const pdfDocument = await pdfjsLib.getDocument({ data: pdfData }).promise;
+      const totalPages = pdfDocument.numPages;
+      let extractedText = "";
+
+      for (let i = 1; i <= totalPages; i++) {
+        const page = await pdfDocument.getPage(i);
+        const content = await page.getTextContent();
+        extractedText += content.items.map((item) => item.str).join(" ") + "\n";
+      }
+
+      resolve(extractedText);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+
+  return text;
+};
 
 function App() {
+  const [resume, setResume] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [companyTitle, setCompanyTitle] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
+
+  const handleFileUpload = async (event) => {
+    const file = event.target.files[0];
+    if (file.type === "application/pdf") {
+      const text = await pdfToText(file);
+      setResume(text);
+    } else {
+      alert("Please upload a PDF file.");
+    }
+  };
+
+  async function generateCoverLetter(resume, jobInfo) {
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:5000/generate_cover_letter",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            resume,
+            job_info: jobInfo,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setCoverLetter(data.cover_letter);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="resumeUpload" className="form-label">
+        Upload your resume (PDF):
+      </label>
+      <input
+        type="file"
+        id="resumeUpload"
+        accept="application/pdf"
+        onChange={handleFileUpload}
+        className="form-control"
+      />
+      <br />
+      <label htmlFor="jobTitle" className="form-label">
+        Job Title:
+      </label>
+      <input
+        type="text"
+        id="jobTitle"
+        value={jobTitle}
+        onChange={(e) => setJobTitle(e.target.value)}
+        className="form-control"
+      />
+      <br />
+      <label htmlFor="companyTitle" className="form-label">
+        Company Title:
+      </label>
+      <input
+        type="text"
+        id="companyTitle"
+        value={companyTitle}
+        onChange={(e) => setCompanyTitle(e.target.value)}
+        className="form-control"
+      />
+      <br />
+      <label htmlFor="jobDescription" className="form-label">
+        Job Description:
+      </label>
+      <textarea
+        id="jobDescription"
+        value={jobDescription}
+        onChange={(e) => setJobDescription(e.target.value)}
+        className="form-control"
+      ></textarea>
+      <br />
+      <button
+        onClick={() =>
+          generateCoverLetter(resume, {
+            jobTitle,
+            companyTitle,
+            jobDescription,
+          })
+        }
+        className="btn btn-primary"
+      >
+        Generate Cover Letter
+      </button>
+
+      {coverLetter && (
+        <div>
+          <h3>Cover Letter:</h3>
+          <div className="cover-letter">{coverLetter}</div>
+        </div>
+      )}
     </div>
   );
 }
